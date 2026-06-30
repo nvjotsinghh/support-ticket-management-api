f73ba3b9038072e47b9f415627f94b306033f55b
nvjotsinghh <navjotsingh5@academic.rrc.ca>
2026-07-01 01:45:31 +0530

    feat: implement update ticket controller

--- src/api/v1/controllers/ticketController.ts
+++ src/api/v1/controllers/ticketController.ts
@@ -8,6 +8,11 @@ const validPriorities = [
   "medium",
   "low",
 ];
+const validStatus = [
+  "open",
+  "in-progress",
+  "resolved",
+];
 
 export const getAllTickets = (_req: Request, res: Response): void => {
   res.status(HTTP_STATUS.OK).json({
@@ -72,5 +77,60 @@ export const createTicket = (req: Request, res: Response): void => {
   res.status(HTTP_STATUS.CREATED).json({
     success: true,
     data: ticket,
+  });
+};
+
+export const updateTicket = (
+  req: Request,
+  res: Response
+): void => {
+  const id = Number(req.params.id);
+
+  const existingTicket = ticketService.getTicketById(id);
+
+  if (!existingTicket) {
+    res.status(HTTP_STATUS.NOT_FOUND).json({
+      success: false,
+      message: "Ticket not found",
+    });
+    return;
+  }
+
+  const { priority, status } = req.body;
+
+  if (
+    priority &&
+    !validPriorities.includes(priority)
+  ) {
+    res.status(HTTP_STATUS.BAD_REQUEST).json({
+      success: false,
+      message:
+        "Invalid priority. Must be one of: critical, high, medium, low",
+    });
+
+    return;
+  }
+
+  if (
+    status &&
+    !validStatus.includes(status)
+  ) {
+    res.status(HTTP_STATUS.BAD_REQUEST).json({
+      success: false,
+      message:
+        "Invalid status. Must be one of: open, in-progress, resolved",
+    });
+
+    return;
+  }
+
+  const updatedTicket = ticketService.updateTicket(
+    id,
+    req.body
+  );
+
+  res.status(HTTP_STATUS.OK).json({
+    success: true,
+    data: updatedTicket,
   });
 };
