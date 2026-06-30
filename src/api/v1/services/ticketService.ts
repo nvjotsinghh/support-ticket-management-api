c5a1a0819edf6f85f51e984a26375dfd139810ba
nvjotsinghh <navjotsingh5@academic.rrc.ca>
2026-07-01 01:42:21 +0530

    feat: implement create ticket service

--- src/api/v1/services/ticketService.ts
+++ src/api/v1/services/ticketService.ts
@@ -1,5 +1,5 @@
 import { tickets } from "../../../data/tickets";
-import { Ticket } from "../models/ticket";
+import { Ticket, TicketPriority } from "../models/ticket";
 
 class TicketService {
   getAllTickets(): Ticket[] {
@@ -8,6 +8,28 @@ class TicketService {
 
   getTicketById(id: number): Ticket | undefined {
     return tickets.find((ticket) => ticket.id === id);
+  }
+
+  createTicket(data: {
+    title: string;
+    description: string;
+    priority: TicketPriority;
+  }): Ticket {
+    const newTicket: Ticket = {
+      id:
+        tickets.length > 0
+          ? Math.max(...tickets.map((t) => t.id)) + 1
+          : 1,
+      title: data.title,
+      description: data.description,
+      priority: data.priority,
+      status: "open",
+      createdAt: new Date().toISOString(),
+    };
+
+    tickets.push(newTicket);
+
+    return newTicket;
   }
 }
 
